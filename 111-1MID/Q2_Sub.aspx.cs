b817b1abad42c31757989983c388b28dd1e93dec
35596035 <william82733791@gmail.com>
2022-10-31 11:09:46 +0800

    ok

--- 111-1MID/Q2_Sub.aspx.cs
+++ 111-1MID/Q2_Sub.aspx.cs
@@ -9,7 +9,7 @@ namespace _111_1MID {
     public partial class Q2_Sub : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
             string[] s_Get = new string[] { "tb_Num", "rbl_Phone", "txt_Phone", "dpl_City", "dpl_Area" };
-            string[] s_Menu = new string[] { "保單編號: ", "通訊種類: ", "通訊號碼: ", "所在城市: ", "所在區域: " };
+            string[] s_Menu = new string[] { "保單號碼: ", "通訊種類: ", "通訊號碼: ", "所在城市: ", "所在區域: " };
             for (int i_Ct = 0; i_Ct < s_Get.Length; i_Ct++)
             {
                 if (Request.Form.Get(s_Get[i_Ct]) == null)
